70442eb6dcb904ae68f2ea7b4e62df0fcbcc88d8
IrinaPastushok <84584434+IrinaPastushok@users.noreply.github.com>
2022-11-12 20:44:16 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import View.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] argv)
+	{
+		myFrame f = new myFrame();
+	}
+}
